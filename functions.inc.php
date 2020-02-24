360148639ae02cf62d2e1310ac0a0abac95aa431
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-02-24 12:11:31 +0100

    Functions

---
+++ functions.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Archive Functions
+ *
+ * @package Coordinator\Modules\Archive
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+
+ // include classes
+ require_once(DIR."modules/archive/classes/cArchiveDocument.class.php");
+
+?>
